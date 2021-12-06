730b39c5b95204443d65e3570ece5b7dcc57c57a
mitul521 <77844700+mitul521@users.noreply.github.com>
2021-12-06 19:01:08 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+status="";
+object=[];
+function setup(){
+ canvas= createCanvas(380,380);
+ canvas.center();
+ video= createCapture(VIDEO);
+ video.size(380,380);
+ video.hide()
+}
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+     document.getElementById("status").innerHTML = "Status : Detecting Objects";
+      object_name = document.getElementById("object_name").value; 
+}
+function modelLoaded(){
+    console.log("model is on");
+    status=true;
+}
